d48f38d3bb8f96003048a58c18f2d9c590965c30
Drwei <weishixvn@outlook.com>
2025-11-22 11:30:57 +0800

    "Fix-missing-prepareForDownload-method-declaration-in-MinecraftResourceDownloadTask.h"

--- Natives/MinecraftResourceDownloadTask.h
+++ Natives/MinecraftResourceDownloadTask.h
@@ -8,13 +8,15 @@
 @property NSMutableDictionary* metadata;
 @property(nonatomic, copy) void(^handleError)(void);
 
-// 新增方法声明（用于账户检查）
-- (BOOL)checkAccessWithDialog:(BOOL)show;
-
-- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path;
-- (void)finishDownloadWithErrorString:(NSString *)error;
-
-- (void)downloadVersion:(NSDictionary *)version;
-- (void)downloadModpackFromAPI:(ModpackAPI *)api detail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
-
+// 新增方法声明（用于账户检查）
+- (BOOL)checkAccessWithDialog:(BOOL)show;
+
+- (void)prepareForDownload;
+
+- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path;
+- (void)finishDownloadWithErrorString:(NSString *)error;
+
+- (void)downloadVersion:(NSDictionary *)version;
+- (void)downloadModpackFromAPI:(ModpackAPI *)api detail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
+
 @end
