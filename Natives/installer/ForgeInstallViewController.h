70d08b372719a5271ed2a65b8f0a391eef08a599
Drwei <weishixvn@outlook.com>
2026-06-13 18:32:43 +0800

    Git怎么又被我玩坏了（
    Signed-off-by: Drwei <weishixvn@outlook.com>

--- Natives/installer/ForgeInstallViewController.h
+++ Natives/installer/ForgeInstallViewController.h
@@ -12,6 +12,5 @@ typedef NS_ENUM(NSInteger, ForgeInstallerFlowErrorCode) {
 @property (nonatomic, copy) NSString *gameVersion;
 @property (nonatomic, assign) BOOL isNeoForge;
 @property (nonatomic, copy) void (^completionHandler)(BOOL success, NSString *profileName, id resultOrError);
-@property (nonatomic, assign) NSInteger selectedScheme;
 
 @end
