7c26710a612d220620b85ad85d7ec23370e2cdfd
Drwei <weishixvn@outlook.com>
2026-06-13 19:58:04 +0800

    fix
    Signed-off-by: Drwei <weishixvn@outlook.com>

--- Natives/installer/ForgeInstallViewController.h
+++ Natives/installer/ForgeInstallViewController.h
@@ -11,6 +11,7 @@ typedef NS_ENUM(NSInteger, ForgeInstallerFlowErrorCode) {
 
 @property (nonatomic, copy) NSString *gameVersion;
 @property (nonatomic, assign) BOOL isNeoForge;
+@property (nonatomic, assign) NSInteger selectedScheme;
 @property (nonatomic, copy) void (^completionHandler)(BOOL success, NSString *profileName, id resultOrError);
 
 @end
